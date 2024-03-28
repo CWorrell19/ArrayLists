2106f7d15cc616fdc60da648c3b6413af29ed2fe
CWorrell19 <164057541+CWorrell19@users.noreply.github.com>
2024-03-28 09:35:07 -0700

    Add files via upload

---
+++ program.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4};
+        int[] arr1 = new int[3];
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
+        
+    }
+
+}
